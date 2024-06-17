4a1af2a63a87cf0bf5a4e07e136eda7d930af72b
superteaa <1059175003@qq.com>
2024-06-17 23:27:52 +0800

    勇者斗恶龙rewrite

--- drogan.cpp
+++ drogan.cpp
@@ -1,46 +1,38 @@
 #include <iostream>
-#include <map>
-#include <set>
 #include <vector>
 #include <algorithm>
-#include <queue>
 
 using namespace std;
 
-typedef long long LL;
-const int maxn = 10001;
-int A[maxn], B[maxn];
-
-int main()
-{
+int main (){
+    int head[200001];
+    int soldier[200001];
     int n, m;
     while (cin >> n >> m && n && m){
-        for(int i = 0; i< n; i++){
-            cin>>A[i];
-        }
-        for(int i = 0; i< m; i++){
-            cin>>B[i];
-        }
-        sort(A,A+n);
-        sort(B,B+m);
         int cur = 0;
         int cost = 0;
-        for(int i = 0;i < m; i++){
-            if(B[i]>=A[cur]){
-                cost = cost + B[i];
+        for(int i = 0; i < n; i++){
+            cin >> head[i];
+        }
+        for(int i = 0; i < m; i++){
+            cin >> soldier[i];
+        }
+        sort(head, head + n);
+        sort(soldier, soldier + m);
+        int i = 0;
+        for(i = 0; i < m; i++){
+            if(soldier[i] >= head[cur]){
+                cost += soldier[i];
                 cur++;
                 if(cur == n){
                     break;
                 }
             }
         }
-        if(cur < n){
-            cout << "Loowater is doomed!" <<endl;
-        }
-        else {
-            cout << cost <<endl;
+        if(cur != n){
+            cout << "Loowater is doomed!" << endl;
+        } else {
+            cout << cost << endl;
         }
     }
-
-    return 0;
 }
