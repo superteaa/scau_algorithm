45c2e7aab68e35d176595143f65ba275c605293c
superteaa <1059175003@qq.com>
2024-06-21 16:16:33 +0800

    迷宫2rewrite

--- maze2.cpp
+++ maze2.cpp
@@ -4,90 +4,89 @@
 
 using namespace std;
 
-int n,m,w;
-int sc, sr, ec, er;
-char maze[101][101];
-
-struct pos{
+struct poin{
     int x;
     int y;
     int step;
 };
 
-pos door[101][101];
+int movx[4] = {0, 1, 0, -1};
+int movy[4] = {1, 0, -1, 0};
 
-int nextx[4] = {0, 1, 0, -1};
-int nexty[4] = {1, 0, -1, 0};
-int ans = 0;
-bool flag = false;
-void gomaze(){
-    pos now = {sc,sr,0};
-    queue<pos> q;
-    q.push(now);
-    while (!q.empty()){
-        
-        now = q.front();
-        pos nex = {0,0,now.step};
-        if (now.x == ec && now.y == er){
-            ans = now.step;
-            flag = true;
-            break;
-        }
-        q.pop();
-        if (maze[now.x][now.y] == '2') {
-            maze[now.x][now.y] = '1';
-            int tmp = now.x;
-            now.x = door[now.x][now.y].x;
-            now.y = door[tmp][now.y].y;
-            now.step++;
-            maze[now.x][now.y] = '1';
-            q.push(now);
-        } else {
-            for (int i = 0; i < 4; i++){
+int n, m;
+int w;
 
-                nex.x = now.x + nextx[i];
-                nex.y = now.y + nexty[i];
-                if (nex.x >= 0 && nex.x < n && nex.y >= 0 && nex.y < m && maze[nex.x][nex.y] != '1'){
-                    if(maze[nex.x][nex.y] != '2')
-                        maze[nex.x][nex.y] = '1';
-                    nex.step = now.step + 1;
-                    q.push(nex);
-                }
-                
-            }
-        }
+char mapp[101][101];
+poin tp[101][101];
 
-    }
-}
+poin now;
+int sc, sr, ec, er;
+
+queue<poin> q;
+int ans = -1;
 
 int main(){
     int casenum;
     cin >> casenum;
-    while (casenum--){
-        flag = false;
-        ans = 0;
+    while(casenum--){
+        
         cin >> n >> m;
-
+        
         for(int i = 0; i < n; i++){
             for(int k = 0; k < m; k++){
-                cin >> maze[i][k];
-                door[i][k] = {-1, -1};
+                cin >> mapp[i][k];
             }
         }
+        
+        
         cin >> w;
-        for (int i = 0; i < w; i++) {
+        while(w--){
             int c1, r1, c2, r2;
             cin >> c1 >> r1 >> c2 >> r2;
-            maze[c1][r1] = '2';
-            door[c1][r1] = {c2, r2};
+            tp[c1][r1].x = c2;
+            tp[c1][r1].y = r2;
+            mapp[c1][r1] = '3';
         }
+        
         cin >> sc >> sr >> ec >> er;
-        maze[sc][sr] = '0';
-        gomaze();
-        if (!flag){
+        now = {sc, sr};
+        now.step = 0;
+        ans = -1;
+        q.push(now);
+        while(!q.empty()){
+            now = q.front();
+            q.pop();
+            if(now.x == ec && now.y == er){
+                ans = now.step;
+                break;
+            }
+            if(mapp[now.x][now.y] == '3'){
+                mapp[now.x][now.y] = '1';
+                int tmpx = now.x;
+                now.x = tp[tmpx][now.y].x;
+                now.y = tp[tmpx][now.y].y;
+                now.step++;
+                q.push(now);
+                mapp[now.x][now.y] = '1';
+            } else {
+                for(int i = 0; i < 4; i++){
+                    poin tmp = now;
+                    tmp.x += movx[i];
+                    tmp.y += movy[i];
+                    if(tmp.x < n && tmp.x >=0 && tmp.y < m && tmp.y >= 0 && mapp[tmp.x][tmp.y] != '1'){
+                        tmp.step++;
+                        q.push(tmp);
+                        if(mapp[tmp.x][tmp.y] != '3'){
+                            mapp[tmp.x][tmp.y] = '1';
+                        }
+                    }
+                }
+            }
+        }
+        if(ans != -1){
+            cout << ans << endl;
+        } else {
             cout << "die" << endl;
         }
-            else cout << ans << endl;
-
     }
 }
